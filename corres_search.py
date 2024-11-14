e1ad97b7a65d09f8275876177fdf66f6103322d0
dokyun-kim4 <donny5557@gmail.com>
2024-11-13 20:45:16 -0500

    create helper function files

---
+++ corres_search.py
@@ -0,0 +1,8 @@
+"""
+This file will contain helper functions for the `Correspondence Search` part of the SFM pipeline.
+- Feature extraction
+- Matching
+- Geometric verification
+"""
+
+
---
+++ reconstruction.py
@@ -0,0 +1,9 @@
+"""
+This file will contain helper functions for the `Incremental Reconstruction` part of the SFM pipeline.
+- Initialization
+- Image Registration
+- Triangulation
+- Bundle Adjustment
+- Outlier Filtering
+- Plotting
+"""
