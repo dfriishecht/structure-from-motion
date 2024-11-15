0b4a77f9bb50fbec18bda79a50ad6275a4765732
dokyun-kim4 <donny5557@gmail.com>
2024-11-15 13:31:24 -0500

    add pseudocode for neccessary functions

--- corres_search.py
+++ corres_search.py
@@ -20,15 +20,28 @@ def match_keypoints(desc1, desc2):
     matches = sorted(matches, key = lambda x:x.distance)
     return matches
 
+def calcualte_essential_matrix(K, F):
+    E = None
+    
+    # E = KT * F * K
+
+    return E
+
+def get_epipolar_line(pos1, pos2, F):
+    L1, L2 = None, None
+    # Epipolar restriction
+    # pos2T * F * pos1 = 0
+
+    # compute left and right lines
+    return L1, L2
+
+def get_camera_pose():
+    pass
+
+
 def draw_matches(img1, img2, kp1, kp2, matches, num_matches=None):
     if not num_matches:
         num_matches = len(matches)
         
     img_matched = cv.drawMatches(img1,kp1,img2,kp2,matches[:num_matches],None) # type: ignore
     return img_matched
-
-def calcualte_essential_matrix():
-    pass
-
-def get_camera_pose():
-    pass
