6c15bd1edb82976d55b66948bffa3f61895de48c
dokyun-kim4 <donny5557@gmail.com>
2024-11-15 23:41:01 -0500

    add some docstrings

--- src/corres_search.py
+++ src/corres_search.py
@@ -9,7 +9,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def extract_features(img):
+def extract_features(img:np.ndarray, intr_mat=None, dist_coeff=None) -> tuple:
+    """
+    Extract SIFT feature points from input image; undistort the image if camera parameters are known
+
+    Args:
+        img
+        intr_mat
+        dist_coeff
+
+    """
+    if intr_mat and dist_coeff:
+        img = cv.undistort(img, intr_mat, dist_coeff) # type: ignore
+
     sift = cv.SIFT.create()
     kp, desc = sift.detectAndCompute(img, None) # type: ignore
     return kp,desc
@@ -20,17 +32,35 @@ def match_keypoints(desc1, desc2):
     matches = sorted(matches, key = lambda x:x.distance)
     return matches
 
-def calcualte_essential_matrix(K, F):
+def calculate_fund_matrix(pts1, pts2):
+    """
+    Calculate the fundamental matrix of two images.
+    This matrix describes the epipolar geometry between two images
+
+    Args: 
+        pts1: matched points from image 1
+        pts2: matched points from image 2
+
+    Returns: 
+        F: Fundamental matrix
+        mask: mask indicating which point belong to inliers/outliers (1/0)
+    """
+
+    F,mask = cv.findFundamentalMat(pts1, pts2, cv.RANSAC)
+    return F, mask
+
+
+def estimate_essential_matrix(K, pts1, pts2):
     E = None
     
-    # E = KT * F * K
+    
 
     return E
 
-def get_epipolar_line(pos1, pos2, F):
+def get_epipolar_line(pts1, pts2, F):
     L1, L2 = None, None
     # Epipolar restriction
-    # pos2T * F * pos1 = 0
+    # pts2T * F * pts1 = 0
 
     # compute left and right lines
     return L1, L2
