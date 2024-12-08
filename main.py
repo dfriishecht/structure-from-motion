f51e284d47d310fad9299c048dde8a404077699b
dokyun-kim4 <donny5557@gmail.com>
2024-12-08 14:50:43 -0500

    more main script refactoring, add descriptive comments

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ tot_imgs = len(images) - 2  # Number of image pairs to process
 # Initialize camera matrices and transformations
 pose0 = np.eye(3, 4)  # Initial pose for the first frame
 pose1 = np.zeros((3, 4))  # Placeholder for subsequent poses
-Xtot = np.zeros((1, 3))  # 3D points
+point_cloud = None  # 3D points
 colorstot = np.zeros((1, 3))  # Corresponding colors
 
 
@@ -63,48 +63,63 @@ P2 = np.dot(K, pose1)
 
 # Get 3d points for first image pair
 points_3d = triangulate(P1, P2, left_pts_ref, right_pts_ref)
+error, points_3d, repro_pts = reprojection_error(points_3d, right_pts_ref, pose1, K, homogenity = 0)
 
-print(np.array(points_3d))
-print(points_3d.shape)
-plot_3d(points_3d)
+print(points_3d.dtype, right_pts_ref.dtype)
+print("REPROJECTION ERROR: ", error)
 
-# prev_img = img1
-# # Expand the 3D point cloud by processing the remaining image incrementally
-# for next_img in tqdm(images[2:]):
+point_cloud = points_3d
 
-#     left_pts, right_pts = match_keypoints_flann(prev_img, next_img)
+# plot_3d(point_cloud)
 
-#     # Find common points for PnP
-#     indx1, indx2, temp1, temp2 = common_points(right_pts_ref, left_pts, right_pts)
-#     com_left_pts = left_pts[indx2]
-#     com_pts_ = pts_[indx2]
-#     com_left_pts_ref = left_pts_ref.T[indx1]
-
-#     # Estimate pose using PnP
-#     Rot, trans, com_left_pts, points_3d, com_pts_ = PnP(points_3d[indx1], com_left_pts, K, np.zeros((5, 1), dtype=np.float32), com_pts_, initial=0)
-#     Rtnew = np.hstack((Rot, trans))
-#     Pnew = np.dot(K, Rtnew)
-
-#     # Reprojection error
-#     error, points_3d, _ = ReprojectionError(points_3d, com_left_pts, Rtnew, K, homogenity=0)
-
-#     # Bundle adjustment (optional)
-#     if bundle_adjustment:
-#         points_3d, temp2, Rtnew = BundleAdjustment(points_3d, temp2, Rtnew, K, gtol_thresh)
-#         Pnew = np.dot(K, Rtnew)
-#         error, points_3d, _ = ReprojectionError(points_3d, temp2, Rtnew, K, homogenity=0)
-#     else:
-#         Xtot = np.vstack((Xtot, points_3d[:, 0, :]))
-#         right_pts_ref_reg = np.array(temp2, dtype=np.int32)
-#         colors = np.array([next_img[l[1], l[0]] for l in right_pts_ref_reg.T])
-#         colorstot = np.vstack((colorstot, colors))
-
-#     # Update poses and image data
-#     pose0 = pose1.copy()
-#     P1, P2 = P2, Pnew
+prev_img = img1
+# Expand the 3D point cloud by processing the remaining image incrementally
+for next_img in tqdm(images[2:]):
     
-#     prev_img = next_img
-#     left_pts_ref, right_pts_ref = pts_, left_pts
+    left_pts, right_pts = match_keypoints_flann(prev_img, next_img)
+
+    # Find common points to use for PnP, also filter out unique pts only to triangulate
+    com_idx_left, com_idx_right, unique_pts_left, unique_pts_right = common_points(right_pts_ref, left_pts, right_pts)
+
+    com_pts_ref = right_pts_ref[com_idx_left]
+    com_pts_left = left_pts[com_idx_right]
+    com_pts_right = right_pts[com_idx_right]
+    com_pts_3d = points_3d[com_idx_left]
+    
+
+    # Estimate new image's pose using PnP
+    Rot, trans, points_3d, com_pts_left, com_pts_right = PnP(com_pts_3d, com_pts_left, com_pts_right, K, initial=0)
+    pose_new = np.hstack((Rot, trans))
+    Pnew = np.dot(K, pose_new)
+
+
+    # Reprojection error
+    error, _ , _ = reprojection_error(points_3d, com_pts_left, pose_new, K, homogenity=0)
+    print(error)
+    
+
+    new_pts_3d = triangulate(P2, Pnew, unique_pts_left, unique_pts_right)
+    error, _ , _ = reprojection_error(new_pts_3d, unique_pts_right, pose_new, K, homogenity=0)
+    print(error)
+
+    print(point_cloud.shape)
+    point_cloud = np.vstack((point_cloud, new_pts_3d))
+    print(point_cloud.shape)
+    plot_3d(point_cloud)
+
+    # Bundle Adjustment
+    points_3d, temp2, Rtnew = bundle_adjustment(points_3d, temp2, Rtnew, K, gtol_thresh)
+    Pnew = np.dot(K, Rtnew)
+    error, points_3d, _ = reprojection_error(points_3d, temp2, Rtnew, K, homogenity=0)
+
+
+    # # Update poses and image data
+    # pose0 = pose1.copy()
+    # P1, P2 = P2, Pnew
+    
+    # prev_img = next_img
+    # left_pts_ref, right_pts_ref = pts_, left_pts
+
 
 #     # Display current image
 #     cv.imshow('image', next_img)
